a4f83a2e7933a684ce2499fce31e8f505396af4c
Tyrone Avnit <tyrone.avnit@standardbank.co.za>
2016-05-30 18:11:59 +0200

    Update tile to allow for setting next, prev, top, and bottom references

--- threes/create-tile.js
+++ threes/create-tile.js
@@ -26,12 +26,48 @@ module.exports = function (number = emptyTile) {
     return attr.number === emptyTile;
   };
 
+  const setEmpty = function () {
+    attr.number = emptyTile;
+  };
+
   const isMoving = function () {
     return attr.isMoving;
   };
 
   const setMoving = function (moving = true) {
     attr.isMoving = moving;
+  };
+
+  const getNext = function () {
+    return attr.next;
+  };
+
+  const setNext = function (tile) {
+    attr.next = tile;
+  };
+
+  const getPrev = function () {
+    return attr.prev;
+  };
+
+  const setPrev = function (tile) {
+    attr.prev = tile;
+  };
+
+  const getTop = function () {
+    return attr.top;
+  };
+
+  const setTop = function (tile) {
+    attr.top = tile;
+  };
+
+  const getBottom = function () {
+    return attr.bottom;
+  };
+
+  const setBottom = function (tile) {
+    attr.bottom = tile;
   };
 
   const update = function () {
@@ -41,10 +77,19 @@ module.exports = function (number = emptyTile) {
   return {
     getNumber,
     isEmpty,
+    setEmpty,
     isRedTile,
     isBlueTile,
     isMoving,
     setMoving,
+    getNext,
+    setNext,
+    getPrev,
+    setPrev,
+    getTop,
+    setTop,
+    getBottom,
+    setBottom,
     update
   };
 };
--- threes/create-tile.spec.js
+++ threes/create-tile.spec.js
@@ -12,6 +12,14 @@ test('tile has the correct properties', t => {
   t.true(!tile.isBlueTile());
 });
 
+test('sets tile to empty', t => {
+  const tile = createTile(1);
+  t.true(tile.getNumber() === 1);
+  tile.setEmpty();
+  t.true(tile.isEmpty());
+  t.true(tile.getNumber() === 0);
+});
+
 test('sets tile to moving', t => {
   const tile = createTile(2);
   tile.setMoving();
@@ -22,6 +30,38 @@ test('stops tile from moving', t => {
   const tile = createTile(2);
   tile.setMoving(false);
   t.true(!tile.isMoving());
+});
+
+test('sets and gets the next tile', t => {
+  const tile = createTile(2);
+  const nextTile = createTile(3);
+  t.true(!tile.getNext());
+  tile.setNext(nextTile);
+  t.is(tile.getNext(), nextTile);
+});
+
+test('sets and gets the previous tile', t => {
+  const tile = createTile(2);
+  const prevTile = createTile(3);
+  t.true(!tile.getPrev());
+  tile.setPrev(prevTile);
+  t.is(tile.getPrev(), prevTile);
+});
+
+test('sets and gets the top tile', t => {
+  const tile = createTile(2);
+  const topTile = createTile(3);
+  t.true(!tile.getTop());
+  tile.setTop(topTile);
+  t.is(tile.getTop(), topTile);
+});
+
+test('sets and gets the bottom tile', t => {
+  const tile = createTile(2);
+  const bottomTile = createTile(3);
+  t.true(!tile.getBottom());
+  tile.setBottom(bottomTile);
+  t.is(tile.getBottom(), bottomTile);
 });
 
 test('upgrades a normal tile with the correct value', t => {
